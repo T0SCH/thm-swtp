e6a4b410d6352f4062bd390f04af2ca4001dbaea
Chris <heinzchris1@gmx.de>
2026-05-21 10:24:23 +0200

    feat: adds user profile page with dummy data.

--- web/ideacamp/src/app/app.routes.ts
+++ web/ideacamp/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {UserProfile} from './pages/user-profile/user-profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'profile', component: UserProfile}
+];
