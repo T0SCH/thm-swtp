23ee98124c9d51bac674c0db12a327fddb8b7250
Chris <heinzchris1@gmx.de>
2026-05-21 13:23:52 +0200

    feat: adds basic layout and styling for the project contact requests page. Currently only with dummy data. Adds route /contact-requests

--- web/ideacamp/src/app/app.routes.ts
+++ web/ideacamp/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {ContactRequests} from './pages/contact-requests/contact-requests';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'contact-requests', component: ContactRequests}
+];
