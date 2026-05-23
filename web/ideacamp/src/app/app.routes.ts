c8acdb2fd4e084cce56c817440570a0705cfdbe3
Chris <166599516+chrishnz@users.noreply.github.com>
2026-05-23 20:13:29 +0200

    fix: added missing ","

--- web/ideacamp/src/app/app.routes.ts
+++ web/ideacamp/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import {UserProfile} from './pages/user-profile/user-profile';
 import {Impressum} from '../pages/impressum/impressum';
 
 export const routes: Routes = [
-  {path: 'profile', component: UserProfile}
+  {path: 'profile', component: UserProfile},
   {path: 'impressum',component: Impressum}
 ];
