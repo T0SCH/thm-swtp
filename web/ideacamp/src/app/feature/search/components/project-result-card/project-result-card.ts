4f3cca22131a1c6bacac619db14c257d140a73fc
Halit Cinar <halit.cinar@mni.thm.de>
2026-05-26 23:58:22 +0200

    fix: remove unused computed and signal imports in project-result-card

--- web/ideacamp/src/app/feature/search/components/project-result-card/project-result-card.ts
+++ web/ideacamp/src/app/feature/search/components/project-result-card/project-result-card.ts
@@ -1,4 +1,4 @@
-import { Component, Input, computed, signal } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ProjectSearchResult } from '../../models/project-search-result.model';
 
 @Component({
