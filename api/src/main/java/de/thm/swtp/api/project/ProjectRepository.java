a485fc2ec449d16f48e1eeb4edc85e179a5c0308
Danylo Konovalenko <danylokonovalenko49@gmail.com>
2026-05-28 02:48:29 +0200

    suppress warn of jpa func

--- api/src/main/java/de/thm/swtp/api/project/ProjectRepository.java
+++ api/src/main/java/de/thm/swtp/api/project/ProjectRepository.java
@@ -6,10 +6,18 @@ import java.util.Optional;
 import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<ProjectEntity, UUID> {
+
     boolean existsByName(String name);
     boolean existsByNameAndIdNot(String name, UUID id);
 
     Optional<ProjectEntity> findByProjectUrl(String projectUrl);
 
+    /**
+     * Finds all projects owned by the user with the given username that are not marked as deleted, ordered by creation date descending.
+     *
+     * @param username the username of the project owner
+     * @return a list of ProjectEntity objects matching the criteria
+     */
+    @SuppressWarnings("checkstyle:MethodName")
     List<ProjectEntity> findAllByOwner_UsernameAndDeletedAtIsNullOrderByCreatedAtDesc(String username);
 }
